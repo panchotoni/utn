eb7e0bbf9d11e6a7cc7c9145115182a7fd59da7e
Pancho <panchotonidandel97@gmail.com>
2022-04-19 20:41:32 -0300

    agregar Phyton

---
+++ Phyton/leccion1-variable/main.py
@@ -0,0 +1,13 @@
+n = 3
+print(n)
+n = "HOLA MUNDO"
+print(n)
+n = 3.0
+print(n)
+x = 10
+y = 2
+z = x + y
+print(z)
+print(id(x))
+# ID: Es el numero de memoria donde se guardan las variables, se muestras las ultimas 3 ej: x134,y cambian cada vez que se ejecuta.
+#En una misma variable se pueden guardar un monton de datos y se van a mostrar todas y no es necesario que se identifiquen la variable
