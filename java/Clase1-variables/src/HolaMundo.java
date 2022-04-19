61dc1b8204720be1b89f0bfc0db1469b34c254db
Pancho <panchotonidandel97@gmail.com>
2022-04-19 20:39:48 -0300

    agregar java

---
+++ java/Clase1-variables/src/HolaMundo.java
@@ -0,0 +1,16 @@
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+
+        int miVariable = 10;
+        System.out.println(miVariable);
+        miVariable = 5;
+        System.out.println(miVariable);
+        String miVariableCadena = "Bienvenidos";
+        System.out.println(miVariableCadena);
+        miVariableCadena = "Sigamos creciendo";
+        System.out.println(miVariableCadena);
+
+    }
+}
