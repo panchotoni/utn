c322009797c435a20d2843817607775fa2f705f4
Pancho <panchotonidandel97@gmail.com>
2022-04-26 19:29:55 -0300

    Ultimo commit

--- Phyton/leccion1-variable/main.py
+++ Phyton/leccion1-variable/main.py
@@ -11,3 +11,37 @@ print(z)
 print(id(x))
 # ID: Es el numero de memoria donde se guardan las variables, se muestras las ultimas 3 ej: x134,y cambian cada vez que se ejecuta.
 #En una misma variable se pueden guardar un monton de datos y se van a mostrar todas y no es necesario que se identifiquen la variable
+
+
+
+#MI GRUPO FAVORITO
+miGrupoFavorito = "Patricio rey y los redonditos de ricota"+", "+"Soundgarden"
+carac = "son buenas bandas"
+print("Mi grupo favorito es: "+miGrupoFavorito, "porque",carac)
+
+#INT PARA CADENAS
+
+num1 = "5"
+num2 = "6"
+print(int(num1) + int(num2))
+
+#TIPOS BOOLEANOS (BOOL)
+
+miBooleano = 3 > 2
+print(miBooleano)
+
+#Si es verdadero entra directamente al primero, si es falso al ELSE>
+if miBooleano:
+    print("El resultado es verdadero")
+else:
+    print("El resultado es falso")
+
+
+#Ejercicios CAMPUS
+dia = input("Como estuvo tu dia del 1 al 10?")
+print("Mi dia estuvo de: ", dia)
+
+titulo = input("Cual es el nombre del libro?")
+autor = input("Cual es el autor del libro?")
+
+print(titulo, "fue escrito por", autor)
--- java/Clase1-variables/src/HolaMundo.java
+++ java/Clase1-variables/src/HolaMundo.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class HolaMundo {
     public static void main(String[] args) {
@@ -12,5 +13,30 @@ public class HolaMundo {
         miVariableCadena = "Sigamos creciendo";
         System.out.println(miVariableCadena);
 
+        var usuario = "Pancho";
+        var apellido = "Toni";
+        var union = (usuario + " " + apellido);
+        System.out.println(union);
+
+        var a = 8;
+        var b = 4;
+        System.out.println(usuario + (a + b));
+
+        var nombre = "Natalia";
+        System.out.println("\nNueva linea: \n" + nombre); //SALTO DE LINEA
+        System.out.println("Tabulador: \t" + nombre); //TABULADOR PARA MOVER EL TEXTO
+        System.out.println("\t\t.:MENU:.");
+        System.out.println("Retroceso: \b" + nombre); //Retroceso, es decir quitar un espacio hacia atras o una caracter que este detras
+        System.out.println("Comillas simples: '" + nombre + "'");
+        System.out.println("Comillas dobles: \""+ nombre+"\"");
+
+        //Clase Scanner: SE USA PARA GUARDAR LA ENTRADA DE DATOS EN LA VARIABLE QUE ASIGNEMOS
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("\nDigite su nombre: ");
+        var usuario2 = entrada.nextLine();
+        System.out.println("Usiario2 = "+ usuario2);
+        System.out.println("Escriba el titulo: ");
+        var titulo2 = entrada.nextLine();
+        System.out.println("Resultado: "+ titulo2 +" "+ usuario2);
     }
 }
