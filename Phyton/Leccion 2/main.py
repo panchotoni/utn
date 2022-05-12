4b9fdb2a4f5e003ce9b36c9070fb0665a0e889f0
Pancho <panchotonidandel97@gmail.com>
2022-05-11 21:54:46 -0300

    Leccion 2

---
+++ Phyton/Leccion 2/main.py
@@ -0,0 +1,8 @@
+#IF / ELSE
+condicion = "ASD"
+if condicion == True:
+    print("Condicion verdadera")
+elif condicion == False:
+    print("Condicion falsa")
+else:
+    print("Condicion sin especificar")
